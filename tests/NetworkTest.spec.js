9d7b43d3494d640469b6803965c35c9032842d30
KRISHNA-3520 <kjamadar26@gmail.com>
2023-07-25 17:18:20 +0530

    added request and response intercepting code + logging req resp code

--- tests/NetworkTest.spec.js
+++ tests/NetworkTest.spec.js
@@ -6,6 +6,8 @@ const loginPayload = {
   userPassword: "Sachin@200",
 };
 
+const fakePayload = { data: [], message: "No Orders" };
+
 const orderPayload = {
   orders: [{ country: "Benin", productOrderedId: "6262e95ae26b7e1a10e89bf0" }],
 };
@@ -25,27 +27,23 @@ test("Browser Context Playwright test", async ({ page }) => {
 
   await page.goto("https://rahulshettyacademy.com/client");
 
+  //altering response
+  await page.route(
+    "https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/6484119a568c3e9fb18365c1",
+    async (route) => {
+      //intercepting response - API response --> browser-->render data on frontend
+      const response = await page.request.fetch(route.request());
+      let body = fakePayload;
+      route.fulfill({
+        response,
+        body,
+      });
+    }
+  );
+  await page.pause();
   //click on ORDERS button
   await page.locator('[routerlink*="myorders"]').click();
 
-  //search placed order id in Orders Tab
-  await page.locator("tbody").waitFor();
-
-  //get row count
-  const rows = await page.locator("tbody tr");
-
-  for (let i = 0; i < (await rows.count()); i++) {
-    //get order id from row
-    const rowOrderId = await rows.nth(i).locator("th").textContent();
-    if (response.orderID.includes(rowOrderId)) {
-      //click on View button of matched order id
-      await rows.nth(i).locator("button").first().click();
-      break;
-    }
-  }
-
-  const orderIdfromDetailsPage = await page.locator(".col-text").textContent();
-  expect(response.orderID.includes(orderIdfromDetailsPage)).toBeTruthy();
-
+  console.log(await page.locator(".mt-4").textContent());
   // await page.pause();
 });
